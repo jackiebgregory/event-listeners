a1cf21e7fd7ebc0eff14235f76dc4db3826ccefe
Jackie Gregory <jackiebgregory7@gmail.com>
2021-01-23 12:01:40 -0600

    more things

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log(class jan. 23)
+//console.log('class jan. 23')
+
+const button = document.querySelector('#main-btn');
+const container = document.querySelector('#container');
+
+//button.addEventListener('click', (event)=> {
+//  console.log(`'You clicked ${event.target.id}!'`);
+//  console.log(event);
+//})
+
+const whatIsTheId = (event) => {
+  console.log(`You clicked ${event.target.id}!`);
+
+  if (event.target.id === 'main-btn') {
+    container.innerHTML = 'You clicked the MAIN BUTTON';
+  } else {
+    container.innerHTML = 'You clicked the OTHER BUTTON';
+  }
+}
+
+button.addEventListener('click', whatIsTheId);
+
+// TARGET 'other-btn' and on click, run whatIsTheId
+const button2 = document.querySelector('#other-btn');
+
+button2.addEventListener('click', whatIsTheId);
