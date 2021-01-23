02af97942070e5c61c5fb3bce41d3afe7a842cb8
Jackie Gregory <jackiebgregory7@gmail.com>
2021-01-23 11:08:55 -0600

    callbacks

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log(class jan. 23)
+//console.log('class jan. 23');
+
+function saysHi (user) {
+  return `Hi ${user}!`;
+}
+function saysBye(user) {
+  return `Bye ${user}!`;
+}
+// A FUNCTION THAT TAKES IN a function as an argument
+function createGreeting(user, cb) {
+  return cb(user);
+}
+// Console logging the return statements of each callback
+console.log(createGreeting('Lauren', saysHi));
+console.log(createGreeting('Lauren', saysBye));
